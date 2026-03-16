84f5b4d97e8eb45f885a1f52ecee8dd0983f022f
Giovanni Flore <flore.giovanni.scripting@outlook.com>
2026-03-16 12:32:48 +0100

    CH2 End

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,7 +1,27 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+	"os/signal"
+
+	amqp "github.com/rabbitmq/amqp091-go"
+)
 
 func main() {
 	fmt.Println("Starting Peril server...")
+	connection := "amqp://guest:guest@localhost:5672/"
+	conn, err := amqp.Dial(connection)
+	if err != nil {
+		fmt.Println("Error when Dialing connection.")
+		return
+	}
+	defer conn.Close()
+	fmt.Println("Connection succedeed.")
+
+	signalChan := make(chan os.Signal, 1)
+	signal.Notify(signalChan, os.Interrupt)
+	<-signalChan
+	fmt.Println()
+	fmt.Println("Interrupt received, shutting down Peril.")
 }
